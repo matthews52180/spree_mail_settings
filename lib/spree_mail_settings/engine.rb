5f263b19ef79c3904436515ff578d9af36706c7c
LEON MUCHOKI WANGECHI <muchokileon@gmail.com>
2020-04-11 10:31:43 +0300

    overrides
    test

--- lib/spree_mail_settings/engine.rb
+++ lib/spree_mail_settings/engine.rb
@@ -5,10 +5,10 @@ module SpreeMailSettings
     engine_name 'spree_mail_settings'
 
     def self.activate
-      #Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-      #  require_dependency(c) #Rails.configuration.cache_classes ? require(c) : load(c)
-      #end
-      Dir.glob(Rails.root + 'app/**/*_decorator*.rb').each do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+      Dir.glob(Rails.root + "app/overrides/**/*_override*.rb").each do |c|
         require_dependency(c)
       end
     end
