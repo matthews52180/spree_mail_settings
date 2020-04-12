b4e6a23942cd79dc7493fff78e2456cbc4f7eefc
LEON MUCHOKI WANGECHI <muchokileon@gmail.com>
2020-04-12 09:08:35 +0300

    update test

--- lib/spree_mail_settings/engine.rb
+++ lib/spree_mail_settings/engine.rb
@@ -6,10 +6,6 @@ module SpreeMailSettings
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    initializer "spree_mail_settings.environment", before: :load_config_initializers do |app|
-      SpreeMailSettings::Config = SpreeMailSettings::Configuration.new
-    end
-
     
     def self.activate
       #Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
